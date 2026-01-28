b5625c7aad969d123463b40f7a29da24abebf474
maliksaadaliazhar <maliksaadaliazhar@gmail.com>
2026-01-28 23:31:14 +0500

    updated requirements

---
+++ src/processor.py
@@ -0,0 +1,8 @@
+import os
+import json
+
+# this will contain these two methods 
+# - parse_document --- that will call parse api of ade
+# - extract_document --- that will call extract api of ade
+# - a schema will be defined
+# - other functionalities will be considered later
