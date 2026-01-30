94a3dc85dfc72d3a7d0cc8a3998c86b6cb2c03a8
maliksaadaliazhar <maliksaadaliazhar@gmail.com>
2026-01-30 23:23:20 +0500

    debug

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import os
 import pandas as pd
 from src.receiptProcessor import process_receipt
 from src.auditor import Auditor
@@ -10,10 +11,12 @@ def main():
 
     uploaded_file = st.file_uploader("Upload Receipt")
     if uploaded_file:
-        with open("temp.jpg", "wb") as f:
+        file_ext = os.path.splitext(uploaded_file.name)[1]
+        temp_filename = f"temp{file_ext}"
+        with open(temp_filename, "wb") as f:
             f.write(uploaded_file.getbuffer())
 
-        receipt = process_receipt("temp.jpg")
+        receipt = process_receipt(temp_filename)
 
         auditor.audit_receipt(receipt)
 
@@ -28,10 +31,16 @@ def main():
 
         # ====== checking if receipt violated any rule ===========
         if receipt.flagged:
-            st.error(f"Fraud Detected : {receipt.flag_reason}")
+            st.error(f"Fraud Detected: {receipt.flag_reason}")
             boxes = receipt.flagged_boxes
-            annotated_image = draw_bounding_boxes("temp.jpg", boxes)
-            st.image(annotated_image, caption="Evidence of Violation", use_container_width=True)
+            
+            if not boxes:
+                st.warning("Violation detected, but could not locate item on image.")
+                st.image(temp_filename, caption="Receipt Image", use_container_width=True)
+            else:
+                st.info(f"Drawing {len(boxes)} red boxes...")
+                annotated_image = draw_bounding_boxes(temp_filename, boxes)
+                st.image(annotated_image, caption="Evidence of Violation", use_container_width=True)
         else:
             st.success("Receipt Approved")
             st.image("temp.jpg", caption="Receipt Image", use_container_width=True)
