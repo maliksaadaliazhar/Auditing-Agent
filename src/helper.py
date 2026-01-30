bb42b2d8fc6df9bf3036db90f06e1c525c67ed7b
maliksaadaliazhar <maliksaadaliazhar@gmail.com>
2026-01-30 23:07:07 +0500

    debug

--- src/helper.py
+++ src/helper.py
@@ -5,13 +5,14 @@ def draw_bounding_boxes(image_path: str, boxes: list) -> Image:
     draw = ImageDraw.Draw(image)
     
     image_width, image_height = image.size
-    absolute_box = [
+    
+    for box in boxes:
+        absolute_box = [
         box[0] * image_width,
         box[1] * image_height,
         box[2] * image_width,
         box[3] * image_height
     ]
-    for box in boxes:
         draw.rectangle(absolute_box, outline="red", width=5)
     
     return
